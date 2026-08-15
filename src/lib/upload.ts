d10f1f08b669ea905a4f0a4b737b61e26b1232e9
Dezl00 <dezl57740@gmail.com>
2026-08-15 19:55:46 +0300

    Add fallback Cloudinary credentials to fix upload error

--- src/lib/upload.ts
+++ src/lib/upload.ts
@@ -2,9 +2,9 @@ import { v2 as cloudinary } from 'cloudinary';
 
 function configureCloudinary() {
   cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "dw7senbmg",
+    api_key: process.env.CLOUDINARY_API_KEY || "158265941332155",
+    api_secret: process.env.CLOUDINARY_API_SECRET || "mBJcsqsgRlp9nuR0rMAquNp6E8s",
   });
 }
 
